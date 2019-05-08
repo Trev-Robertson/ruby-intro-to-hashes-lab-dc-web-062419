d4d57f5bd769e5902afcf37fe37efd8237eb1c40
Trevahr Robertson <trevahr@yahoo.com>
2019-05-08 01:52:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,5 +23,13 @@ end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	  {
+      railroads: {
+        pieces: 4, 
+        names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}}, 
+        rent_in_dollars: {one_piece_with_third_tier: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 400} 
+        
+      }
+      
+    }
 end
